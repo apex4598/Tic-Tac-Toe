c633a5134f41a2839cf5c7687a231698728fdd8d
Apex4598 <login2ajit@gmail.com>
2022-05-25 09:18:35 +0530

    Added Fuctionality to for user input

--- src/com/bridgelabz/workshop/TicTocToe.java
+++ src/com/bridgelabz/workshop/TicTocToe.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.workshop;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class TicTocToe {
+    public static char player, computer;
+
     public static char[] createboard()
     {
         char[] board = new char [10];
@@ -12,10 +15,31 @@ public class TicTocToe {
         }
         return board;
     }
+        public void playerchoice(){
+
+            System.out.println("Enter X or O");
+            Scanner obj = new Scanner(System.in);
+            player = obj.next().charAt(0);
+
+            if (player == 'X' || player == 'x' )
+            {
+                computer = 'O';
+                System.out.println( "Computer " +computer);
+
+            } else if (player == 'O' || player == 'o') {
+                computer = 'X';
+                System.out.println("Computer " +computer);
+
+            }
+        }
 
 
     public static void main(String[] args) {
         System.out.println( "Welocme to Tic tac Toe ");
         createboard();
-    }
+        TicTocToe obj2 = new TicTocToe();
+        obj2.playerchoice();
+        
+
+        }
 }
