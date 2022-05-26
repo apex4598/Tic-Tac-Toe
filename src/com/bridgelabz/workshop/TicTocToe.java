6cd3b6cbd738840c62e3e2e77e941da50af3678f
Apex4598 <login2ajit@gmail.com>
2022-05-26 09:39:59 +0530

    Dispaly Board

--- src/com/bridgelabz/workshop/TicTocToe.java
+++ src/com/bridgelabz/workshop/TicTocToe.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.workshop;
 
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 
 public class TicTocToe {
@@ -16,7 +16,6 @@ public class TicTocToe {
         return board;
     }
         public void playerchoice(){
-
             System.out.println("Enter X or O");
             Scanner obj = new Scanner(System.in);
             player = obj.next().charAt(0);
@@ -32,14 +31,29 @@ public class TicTocToe {
 
             }
         }
+    public void board(){
 
+        for (int i = 0; i < 3; i++){
+            for (int j = 0 ; j < 3; j++){
+
+                System.out.println("---");
+            }
+            System.out.println("  ");
+            for (int j = 0; j < 3; j++){
+
+                System.out.println("| ");
+
+            }
+
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println( "Welocme to Tic tac Toe ");
         createboard();
         TicTocToe obj2 = new TicTocToe();
         obj2.playerchoice();
-        
+        obj2.board();
 
         }
 }
